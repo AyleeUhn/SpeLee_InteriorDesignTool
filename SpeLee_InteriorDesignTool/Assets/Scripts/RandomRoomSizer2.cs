c3b206a6fdfe4c0950b45b3b5302961a2652c004
Spencer <xerron147@gmail.com>
2018-11-12 12:35:37 -0700

    Room BlendShapes C#

--- SpeLee_InteriorDesignTool/Assets/Scripts/RandomRoomSizer2.cs
+++ SpeLee_InteriorDesignTool/Assets/Scripts/RandomRoomSizer2.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class RandomRoomSizer2 : MonoBehaviour
 {
 
+	private SkinnedMeshRenderer sMR;
 	public Rigidbody rb;
 	public int stepBounds = 4;
 	private int step;
@@ -12,12 +13,38 @@ public class RandomRoomSizer2 : MonoBehaviour
 	public int lowerBounds = 16;
 	public int upperBounds = 40;
 
-	private int roomWidth;
-	private int roomLength;
+	//private int roomWidth;
+	//private int roomLength;
+
+	public GameObject NorthDoor;
+	public GameObject EastDoor;
+	public GameObject SouthDoor;
+	public GameObject WestDoor;
+
+	private SkinnedMeshRenderer NDBlend;
+	private SkinnedMeshRenderer EDBlend;
+	private SkinnedMeshRenderer SDBlend;
+	private SkinnedMeshRenderer WDBlend;
+
+	
+	void Awake()
+	{
+		sMR = GetComponent<SkinnedMeshRenderer>();
+		
+		NDBlend = NorthDoor.GetComponent<SkinnedMeshRenderer>();
+		EDBlend = EastDoor.GetComponent<SkinnedMeshRenderer>();
+		SDBlend = SouthDoor.GetComponent<SkinnedMeshRenderer>();
+		WDBlend = WestDoor.GetComponent<SkinnedMeshRenderer>(); 
+		
+	}
 	
 	// Use this for initialization
 	void Start ()
 	{
+		GameObject northDoor = NorthDoor;
+		GameObject eastDoor = EastDoor;
+		GameObject southDoor = SouthDoor;
+		GameObject westDoor = WestDoor;
 
 		// rb = GetComponent<Rigidbody>();
 		
@@ -33,8 +60,14 @@ public class RandomRoomSizer2 : MonoBehaviour
 			
 		}
 		roomLength = Random.Range(lowerBounds, upperBounds);
-		transform.localScale = new Vector3( roomWidth , 1 , roomLength );
-		transform.position = new Vector3(0,0,(-roomLength));
+		// transform.localScale = new Vector3( roomWidth , 1 , roomLength );
+		//10 & 11
+		sMR.SetBlendShapeWeight(8, roomWidth*10);
+		NDBlend.SetBlendShapeWeight(8, roomWidth * 10);
+		sMR.SetBlendShapeWeight(9, roomLength*10);
+		eastDoor.sMR.SetBlendShapeWeight(9, roomLength * 10);
+
+		//transform.position = new Vector3(0,0,(/*(*/roomLength/* * 14) - 100*/));
 	}
 	
 	// Update is called once per frame
